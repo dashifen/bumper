f6ca119c6ed6277a4e07fdeafdbd2bfdd3ca3c8b
David Dashifen Kees <dashifen@dashifen.com>
2023-09-19 11:22:34 -0400

    parameterized bumper object in bump.php

--- bump.php
+++ bump.php
@@ -2,12 +2,27 @@
 
 namespace Dashifen\Composer;
 
-if (!class_exists('Dashifen\Composer\Bumper')) {
+// by default, we use the bumper object included within this package.  however,
+// sometimes we might want to use an object other than that one.  therefore, we
+// allow the "--object" flag on the command line and assume the value in the
+// next argument is the object we want to use.  PHP will throw an error if it
+// does not exist when we try to instantiate it in the try-block below to help
+// protect us from mistakes.
+
+$object = 'Dashifen\Composer\Bumper';
+foreach ($_SERVER['argv'] as $i => $arg) {
+  if ($arg === '--object') {
+    $object = $_SERVER['argv'][$i + 1];
+    break;
+  }
+}
+
+if (!class_exists($object)) {
   require 'vendor/autoload.php';
 }
 
 try {
-  $bumper = new Bumper();
+  $bumper = new $object;
   $runSimulation = Bumper::flagExists('simulate');
   
   // if we are running a simulation, then we don't want to commit.  but, if
